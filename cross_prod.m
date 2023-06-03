e1b3e1932ed29bbe160a195a1841e5d59136a006
yudhajit <120769655+jjmouse@users.noreply.github.com>
2023-06-02 18:18:30 -0600

    Add files via upload

---
+++ cross_prod.m
@@ -0,0 +1,7 @@
+[Act,CC,Mod,Alpha,Beta,Theta]=readvars("hackathon1.xlsx")
+
+Act_by_Wave = [0,-1,0;-1,0,1;0,0,1;-1,0,0;0,-1,1;0,0,0];
+Wave_by_Net = [1,0,0;0,1,0;,1,1,0];
+Net_by_TT = [0,1,1;1,0,0;1,1,0];
+
+ActScores = Act_by_Wave * Wave_by_Net * Net_by_TT;
